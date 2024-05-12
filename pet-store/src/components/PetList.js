18f20025f6af0e8d796f5178e4f36dfbe17d3672
BhatiaYukta <140514258+BhatiaYukta@users.noreply.github.com>
2024-05-12 21:10:59 +0000

    Pets details display acc to status

--- pet-store/src/components/PetList.js
+++ pet-store/src/components/PetList.js
@@ -1,5 +1,11 @@
 import React from "react"
 import styled from "styled-components"
+
+const Container = styled.div`
+display: flex,
+flex-direction:column;
+`;
+
 const MovieDiv = styled.div`
 display:flex;
 flex-direction:column;
@@ -12,17 +18,28 @@ const CoverImage = styled.img`
 height:362px;
 object-fit:cover; 
 `
+const MovieName = styled.span`
+font-size:18px;
+font-weight:600;
+color:black;
+margin: 15px 0;
+white-space:nowrap;
+text-overflow:ellipsis;
+overflow:hidden;
+`
 
-
-const PetList =(props)=>{
-    // console.log("props",props["pets"]["photoUrls"][0] && props["pets"]["photoUrls"][0].split(',').filter(url => url && url.includes('.png')))
-    // let pngImage= props["pets"]["photoUrls"][0] && props["pets"]["photoUrls"][0].split(',').filter(url => url && url.includes('.png'))
-    // console.log("pngImage",pngImage!==undefined && pngImage.length!==0 && if(Array.isArray(pngImage)){ pngImage[0]})
-    
+const PetList = (props) => {
+   
+    const imageURL = props.pets.photoUrls.filter(url => url && (url.startsWith('http://') || url.startsWith('https://')))
+    console.log(imageURL[0])
     return (
-    <MovieDiv>
-        {/* <CoverImage>src= {pngImage.length!==0 && pngImage}</CoverImage> */}
-    </MovieDiv>
+        <MovieDiv>
+
+            { imageURL[0]!==undefined ? <CoverImage src={imageURL[0]}></CoverImage>: "Image Not Available"}
+
+            <MovieName> Name : {props.pets.name}</MovieName>
+            <MovieName> Status: {props.pets.status}</MovieName>
+        </MovieDiv>
     )
 }
 
